0b45b33a24bab05a7efb1716460068b5ecbfde24
vilinicz <v.ilinicz@gmail.com>
2019-04-22 00:49:19 +0300

    redesign + update costs

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,30 @@
     </div>
 
     <div class="calculator">
+      <div class="district">
+        <label for="district">Выберите округ:</label>
+        <select
+          id="district"
+          v-model="selected"
+          class="input"
+        >
+          <option
+            v-for="(d, i) in districts"
+            :key="i"
+            :value="d"
+          >
+            {{ d.abbr }}
+          </option>
+        </select>
+      </div>
+
       <div
         ref="map"
         class="map"
       >
         <svg
-          :height="height"
-          :viewBox="`0 0 ${width} ${height}`"
-          :width="width"
+          viewBox="0 0 286 327"
+          xmlns="http://www.w3.org/2000/svg"
         >
           <g class="districts">
             <g
@@ -20,14 +36,14 @@
               :key="`path-${i}`"
               :class="{'selected': d === selected}"
               class="d"
-              @click="select(d)"
+              @click="selected = d"
             >
 
               <path
-                :d="geoPath().projection(projection())(d)"
+                :d="d.shape"
                 vector-effect="non-scaling-stroke"
               />
-              <text
+              <!--text
                 :class="`label-${d.properties.id}`"
                 :x="geoPath().projection(projection()).centroid(d)[0]"
                 :y="geoPath().projection(projection()).centroid(d)[1]"
@@ -36,169 +52,148 @@
                 fill="black"
                 text-anchor="middle"
               >{{ d.properties.abbr }}
-              </text>
+              </text-->
             </g>
           </g>
         </svg>
       </div>
 
-      <div class="form">
-        <div class="district">
-          <label for="district">Укажите округ:</label>
-          <select
-            id="district"
-            v-model="selected"
-            class="input"
-          >
-            <option
-              v-for="(d, i) in districts"
-              :key="i"
-              :value="d"
-            >
-              {{ d.properties.abbr }}
-            </option>
-          </select>
-        </div>
-
-        <div class="area">
-          <div class="wrapper">
-            <label for="area">
-              Площадь м<sup>2</sup>
-            </label>
-            <input
-              id="area"
-              v-model="area"
-              :max="areaMax"
-              :min="areaMin"
-              class="input"
-              type="number"
-            >
-          </div>
-          <vue-slider
+      <div class="area">
+        <label for="area">
+          Площадь обьекта:
+        </label>
+        <div class="wrapper">
+          <vue-numeric
+            id="area"
             v-model="area"
-            :contained="true"
-            :height="3"
-            :max="areaMax"
+            class="input"
+            currency=""
+            separator="space"
             :min="areaMin"
-            dot-size="16"
+            :max="areaMax"
           />
+          <span>м2</span>
         </div>
+        <vue-slider
+          v-model="area"
+          :contained="true"
+          :height="3"
+          :max="areaMax"
+          :min="areaMin"
+          tooltip="none"
+          :interval="100"
+          dot-size="16"
+        />
       </div>
+      <a
+        v-smooth-scroll
+        href="#result"
+        class="btn"
+        @click="calculate()"
+      >Рассчитать</a>
     </div>
 
-    <div class="result">
-      Results
+    <div
+      id="result"
+      class="result"
+    >
+      <transition name="fade">
+        <div
+          v-if="calculated"
+          class="calculated"
+        >
+          <div>
+            <div class="label">
+              Текущая стоимость объекта:
+            </div>
+            <div
+              class="value"
+              v-html="costToString(currentCost)"
+            />
+          </div>
+          <div>
+            <div class="label">
+              Стоимость ретауна:
+            </div>
+            <div
+              class="value"
+              v-html="costToString(rebuildCost)"
+            />
+          </div>
+          <div class="total">
+            <div class="label">
+              Стоимость объекта после реновации:
+            </div>
+            <div
+              class="value"
+              v-html="costToString(newCost)"
+            />
+          </div>
+
+          <div class="disclaimer">
+            *Дисклеймер про оферту и все такое вот разное,
+            например две строчки, ну или там
+          </div>
+
+          <a
+            href="#contacts"
+            class="btn"
+          >Обсудить</a>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
 import VueSlider from 'vue-slider-component';
-
-import { geoCentroid, geoMercator, geoPath } from 'd3-geo';
-import mapData from '@/assets/old_moscow_districts_simplified.geo.json';
-
+import VueNumeric from 'vue-numeric';
+import districts from '@/assets/districts.json';
 
 export default {
   name: 'App',
   components: {
     VueSlider,
+    VueNumeric,
   },
 
   data() {
     return {
-      width: 0,
-      height: 0,
-
-      meterCost: 210000,
-
-      districts: [],
+      districts,
       area: 10000,
       areaMin: 1000,
       areaMax: 50000,
 
-      selected: undefined,
-      hovered: undefined,
+      selected: districts[0],
+
+      calculated: false,
+
+      currentCost: 0,
+      rebuildCost: 0,
+      newCost: 0,
     };
   },
 
-  computed: {
-    total() {
-      return Math.round(this.area * this.meterCost * this.coefficient);
-    },
-
-    coefficient() {
-      return ((id) => {
-        switch (id) {
-          case 1:
-            return 2;
-          case 2:
-            return 1.7;
-          case 3:
-            return 1.6;
-          case 4:
-            return 1.4;
-          case 5:
-            return 1.35;
-          case 6:
-            return 1.3;
-          case 7:
-            return 1.45;
-          case 8:
-            return 1.5;
-          case 9:
-            return 1.72;
-          case 10:
-            return 1.2;
-          case 11:
-            return 1.1;
-          case 12:
-            return 1;
-          default:
-            return 1;
-        }
-      })(this.selected.properties.id);
-    },
-  },
-
-  async mounted() {
-    this.height = this.$el.querySelector('.map').clientHeight;
-    this.width = this.$el.querySelector('.map').clientWidth;
-
-    this.districts = mapData.features.sort((a, b) => a.properties.id - b.properties.id);
-    [this.selected] = this.districts;
-  },
-
   methods: {
-    projection() {
-      return geoMercator()
-        .fitSize([this.width, this.height], mapData);
+    calculate() {
+      this.calculated = false;
+      this.currentCost = Math.round(this.area * this.selected.old_meter_cost);
+      this.rebuildCost = Math.round(this.area * this.selected.development_meter_cost);
+      this.newCost = Math.round(this.area * this.selected.new_meter_cost);
+
+      setTimeout(() => {
+        this.calculated = true;
+      }, 800);
     },
 
-    geoPath() {
-      return geoPath();
-    },
-
-    centroid() {
-      return geoCentroid();
-    },
-
-    districtAbbr(string) {
-      return string[0];
-    },
-
-    mouseover(d) {
-      if (d) {
-        this.highlighted = d.properties;
+    costToString(n) {
+      if (n > 1000000000) {
+        return `${Math.round((n / 1000000000) * 10) / 10} <span>млрд. руб</span>`;
       }
-    },
-
-    mouseout() {
-      this.highlighted = undefined;
-    },
-
-    select(d) {
-      this.selected = d;
+      if (n > 1000000) {
+        return `${Math.round((n / 1000000) * 10) / 10} <span>млн. руб</span>`;
+      }
+      return `${Math.round((n / 1000) * 10) / 10} <span>тыс. руб</span>`;
     },
   },
 };
@@ -208,7 +203,7 @@ export default {
   $light: #F2F7FA;
   $gray: #dcdcdc;
   $black: #181818;
-  $red: #EB1E4B;
+  $red: #F8244B;
 
   $primary: $red;
 
@@ -218,39 +213,94 @@ export default {
 
 
   #app {
+    color: $black;
     min-height: 100%;
-    min-width: 100%;
-    background: $light linear-gradient(324.93deg, #EBEFF2 -3.08%, #F2F7FA 91.37%, #F2F7FA 91.37%);
+    background: $light linear-gradient(20.06deg, #D8D8D8 -54.86%, #F2F7FA 53.79%, #FFFFFF 101.94%);
     position: relative;
-    padding: 4vh 4vw;
+    padding: 2rem 1rem;
     display: grid;
     grid-template-columns: 1fr;
-    grid-template-rows: auto 1fr;
+    grid-template-rows:
+      auto
+      minmax(calc(100vh - 4rem - 80px - 60px), auto)
+      minmax(calc(100vh - 2rem - 60px), auto);
+    grid-template-areas: 'header' 'calculator' 'result';
 
-    grid-template-areas:
-      'header'
-      'calculator'
-      'result';
+    @media(min-width: 1024px) {
+      padding: 5vh 6vw;
+      grid-template-columns: 1fr 1fr;
+      grid-column-gap: 6vw;
+      grid-template-rows: auto 1fr;
+      grid-template-areas:
+        'header header'
+        'calculator result';
+    }
+    @media (min-width: 1200px) {
+      padding: 8vh 10vw;
+      grid-column-gap: 10vw;
+    }
+
+    .btn {
+      width: 100%;
+      height: 52px;
+      display: flex;
+      flex-shrink: 0;
+      align-items: center;
+      justify-content: center;
+      padding: 1rem;
+      background-color: $black;
+      color: #fff;
+      margin-top: 1.25rem;
+      font-size: 16px;
+      transition: opacity 0.25s ease;
+      &:hover {
+        opacity: 0.8;
+      }
+      @media(min-width: 768px) {
+        margin-top: 2.5rem;
+      }
+    }
 
 
     .header {
       grid-area: header;
       font-weight: 900;
-      font-size: 32px;
+      font-size: 28px;
       color: $black;
-      line-height: 1;
-      padding-bottom: 10px;
+      line-height: 1.1;
+      padding-bottom: 1rem;
       border-bottom: 2px solid $gray;
+      @media(min-width: 768px) {
+        font-size: 34px;
+      }
+      @media(min-width: 1200px) {
+        font-size: 38px;
+      }
     }
 
 
     .calculator {
       grid-area: calculator;
-      padding: 5vh 0;
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: space-between;
+      overflow: hidden;
+      padding-top: 1.25rem;
 
       .map {
-        height: 300px;
-        margin-left: -2%;
+        margin: auto;
+        overflow: hidden;
+        max-width: 33vh;
+        width: 100%;
+
+        @media(min-width: 1024px) {
+          max-width: 40vh;
+        }
+
+        svg {
+          height: 100%;
+          width: 100%;
+        }
 
         .d {
           fill: $gray;
@@ -261,6 +311,7 @@ export default {
 
           &:hover {
             fill: darken($gray, 5%);
+
             .label {
               fill: $black;
             }
@@ -268,6 +319,7 @@ export default {
 
           &.selected {
             fill: $primary;
+
             .label {
               fill: $light;
             }
@@ -275,7 +327,7 @@ export default {
 
           &:hover, &.selected {
             .label {
-              opacity: 1;
+              // opacity: 1;
             }
           }
         }
@@ -291,47 +343,167 @@ export default {
         }
       }
 
-
-        .district, .area {
+      .district, .area {
+        z-index: 2;
+        label {
+          display: block;
           margin-bottom: 1rem;
+          font-size: 1rem;
+          color: $black;
+        }
+
+        .input {
+          -webkit-appearance: none;
+          background-color: transparent;
+          font-size: 18px;
+          font-weight: 800;
+          line-height: 1;
+        }
+
+        @media(min-width: 768px) {
           label {
-            display: block;
-            margin-bottom: 0.75rem;
-            font-size: 0.9rem;
-            color: $black;
+            font-size: 1.2rem;
           }
-
           .input {
-            -webkit-appearance: none;
-            display: block;
-            background-color: transparent;
-            border-bottom: 2px solid $primary;
             font-size: 24px;
-            line-height: 29px;
-            font-weight: 800;
           }
-        }
-
-        .area > .wrapper {
-          //display: flex;
-          //flex-flow: row wrap;
-          //align-items: center;
-          margin-bottom: 0.75rem;
-
-          input, label {
-            width: auto;
-            flex: 1 1 auto;
-            //margin-bottom: 0;
-          }
-        }
-
-        .area .vue-slider {
-          padding: 8px 4px !important;
         }
       }
 
+      .district {
+        align-self: flex-start;
+        select {
+          background: transparent url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>") no-repeat;
+          background-position-x: 100%;
+          background-position-y: 50%;
+          padding-right: 20px;
+        }
+      }
+
+      .area {
+        text-align: right;
+        margin-top: auto;
+        .wrapper {
+          display: flex;
+          justify-content: flex-end;
+          align-items: flex-end;
+        }
+        .input {
+          text-align: right;
+        }
+        span {
+          margin-left: 0.5rem;
+          font-size: 18px;
+          font-weight: 800;
+          line-height: 1.2;
+          @media(min-width: 768px) {
+            font-size: 24px;
+          }
+        }
+      }
+
+      .area .vue-slider {
+        margin-top: 0.75rem;
+        .vue-slider-rail{
+          position: relative;
+          &:before, &:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: -6px;
+            width: 6px;
+            height: 3px;
+            background-color: $red;
+          }
+          &:after {
+            right: -6px;
+            left: auto;
+            background-color: $bgColor;
+          }
+        }
+        .vue-slider-dot-handle {
+          border-radius: 0;
+          box-shadow: none;
+          background-color: $red;
+        }
+      }
+
+      .btn {
+        background-color: $black;
+      }
+
+      @media(min-width: 768px) {
+        padding-top: 2.5rem;
+      }
+    }
+
     .result {
       grid-area: result;
+      padding-top: 3rem;
+      .calculated {
+        height: 100%;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: space-between;
+      }
+      .label {
+        margin-bottom: 1rem;
+      }
+      .value {
+        font-size: 24px;
+        font-weight: 800;
+        padding-bottom: 0.25rem;
+        margin-bottom: 1.5rem;
+        border-bottom: 2px solid rgba($gray, 0.5);
+        span {
+          font-size: 0.8em;
+        }
+      }
+      .total {
+        margin: auto 0;
+        .value {
+          border-bottom: 0;
+          font-size: 42px;
+          font-weight: 900;
+        }
+      }
+
+      .disclaimer {
+        margin-top: auto;
+        font-style: italic;
+        // To align with area slider
+        padding-bottom: 3px;
+        line-height: 16px;
+
+        font-size: 13px;
+      }
+      .btn {
+        background-color: $red;
+      }
+
+      @media(min-width: 768px) {
+        padding-top: 2.5rem;
+        .label {
+          font-size: 1.2rem;
+        }
+        .value {
+          font-size: 32px;
+        }
+        .total > .value {
+          font-size: 48px;
+        }
+
+        .disclaimer {
+          font-size: 1rem;
+        }
+      }
     }
   }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .25s ease-in-out;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import vueSmoothScroll from 'vue2-smooth-scroll';
 import App from './App.vue';
 import '@/assets/main.scss';
 
+Vue.use(vueSmoothScroll);
 Vue.config.productionTip = false;
 
 new Vue({
